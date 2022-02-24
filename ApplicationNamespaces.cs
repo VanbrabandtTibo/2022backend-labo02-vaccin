f83f2a6a856f8691f06cab45269ba20fb4aee93d
Tibo Vanbrabandt <tibo.vanbrabandt@student.howest.be>
2022-02-24 17:13:39 +0100

    Oef 2 Done

--- ApplicationNamespaces.cs
+++ ApplicationNamespaces.cs
@@ -1,21 +1,12 @@
 global using System;
-
 global using VaccinOefening.Models;
-
 global using VaccinOefening.Repositories;
-
 global using VaccinOefening.Services;
-
 global using VaccinOefening.Configuration;
-
 global using Microsoft.AspNetCore.Diagnostics;
-
 global using VaccinOefening.DTO;
-
 global using AutoMapper;
-
 global using CsvHelper;
 global using System.Globalization;
 global using CsvHelper.Configuration;
-
 global using Microsoft.Extensions.Options;
